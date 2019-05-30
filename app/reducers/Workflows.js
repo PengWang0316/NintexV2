93c05571d4a38e9f6705098358ba6c9b4d5b6a7e
Kevin Wang <rocwang0316@gmail.com>
2019-05-30 00:41:01 -0700

    Updated the data structure for the Workflows state

--- app/reducers/Workflows.js
+++ app/reducers/Workflows.js
@@ -1,11 +1,16 @@
 import { FETCH_WORKFLOWS_BY_USER_SUCCESS } from '../actions/ActionTypes';
 
 const workflows = (
-  state = { isFetched: false, data: [] }, { type, workflows: workflowsByUser },
+  state = { isFetched: false, data: {} }, { type, workflows: workflowsByUser },
 ) => {
   switch (type) {
-    case FETCH_WORKFLOWS_BY_USER_SUCCESS:
-      return { isFetched: true, data: workflowsByUser };
+    case FETCH_WORKFLOWS_BY_USER_SUCCESS: {
+      const workflowsObj = {};
+      workflowsByUser.forEach((workflow) => {
+        workflowsObj[workflow.workflowId] = workflow;
+      });
+      return { isFetched: true, data: workflowsObj };
+    }
     default:
       return state;
   }
