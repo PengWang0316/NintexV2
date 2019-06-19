3a5da1e856e1d6cbaf3bdb55d0e29fd6afcc9ca1
Kevin Wang <rocwang0316@gmail.com>
2019-06-18 19:59:50 -0700

    Updated the data structure for MonitorList state and put it in the persist list.

--- app/reducers/MonitorList.js
+++ app/reducers/MonitorList.js
@@ -6,15 +6,28 @@ const MonitorList = (state = { isFetched: false, data: {} }, {
   switch (type) {
     case FETCH_MONITOR_LIST_SUCCESS: {
       const monitorList = { isFetched: true, data: {} };
+      // The
       workflows.forEach((workflow) => {
-        if (workflow.isMonitored && workflow.isMonitored === 1) monitorList.data[workflow.workflowId] = workflow.tenant;
+        if (workflow.isMonitored && workflow.isMonitored === 1) {
+          monitorList.data[workflow.workflowId] = {
+            tenant: workflow.tenant,
+            instances: {},
+            hasFailure: false,
+          };
+        }
       });
       return monitorList;
     }
     case SWITCH_MONITOR_SUCCESS: {
       const monitorList = { isFetched: true, data: { ...state.data } };
       if (monitorList.data[workflowId]) delete monitorList.data[workflowId];
-      else monitorList.data[workflowId] = tenant;
+      else {
+        monitorList.data[workflowId] = {
+          tenant,
+          instances: {},
+          hasFailure: false,
+        };
+      }
       return monitorList;
     }
     default:
--- app/store/ConfigureStore.js
+++ app/store/ConfigureStore.js
@@ -12,7 +12,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: [],
+  whitelist: ['monitorList'],
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
