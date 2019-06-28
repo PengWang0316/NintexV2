a95d7bebd5fa0eaaf830a74e17dcfe6aaea6c1b8
Kevin Wang <rocwang0316@gmail.com>
2019-06-27 22:11:10 -0700

    Changed the interface

--- app/store/Workflows/types.ts
+++ app/store/Workflows/types.ts
@@ -1,24 +1,30 @@
-interface BaseWorkflowDate {
+interface Workflow {
   workflowId: string;
   workflowName: string;
   publishDate: string;
   publisher: string;
   tag: string | null;
+  locationPath?: string;
+  isActive?: number;
+  tenant?: string;
+  isMonitored?: number;
 }
 
-interface OfficeWorkflow extends BaseWorkflowDate {
-  locationPath: string;
-}
+// interface OfficeWorkflow extends BaseWorkflowDate {
+//   locationPath: string;
+// }
 
-interface NwcWorkflow extends BaseWorkflowDate {
-  isActive: number;
-  tenant: string;
-  isMonitored: number;
-}
+// interface NwcWorkflow extends BaseWorkflowDate {
+//   isActive: number;
+//   tenant: string;
+//   isMonitored: number;
+// }
+
+// export type Workflow = OfficeWorkflow | NwcWorkflow;
 
 export interface Workflows {
   isFetched: boolean;
   data: {
-    [workflowId: string]: OfficeWorkflow | NwcWorkflow;
+    [workflowId: string]: Workflow;
   };
 }
