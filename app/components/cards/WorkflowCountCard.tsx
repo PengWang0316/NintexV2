5b7ebdfb108c2de1f0729e1dc599b5451c344b89
Kevin Wang <rocwang0316@gmail.com>
2019-06-30 16:49:55 -0700

    Converted the WorkflowCountCard component to the ts format.

--- app/components/cards/WorkflowCountCard.tsx
+++ app/components/cards/WorkflowCountCard.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import React, { useEffect, memo } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
 import { red } from '@material-ui/core/colors';
 import { Stars } from '@material-ui/icons';
@@ -8,16 +7,23 @@ import I18n from '@kevinwang0316/i18n';
 
 import DashboardNumCard from './DashboardNumCard';
 import { fetchWorkflowCount as fetchWorkflowCountAction } from '../../store/WorkflowCount/actions';
+import { AppState } from '../../store/ConfigureStore';
 
-const styles = {
+interface Props {
+  workflowCount: number | null;
+  fetchWorkflowCount: () => void;
+}
+
+const useStyles = makeStyles({
   cardIcon: {
     fontSize: 36,
     color: red[500],
   },
-};
-let isFetching;
+});
+let isFetching = false;
 
-export const WorkflowCountCard = ({ classes, workflowCount, fetchWorkflowCount }) => {
+export const WorkflowCountCard = ({ workflowCount, fetchWorkflowCount }: Props) => {
+  const classes = useStyles({});
   useEffect(() => {
     if (!workflowCount && !isFetching) {
       fetchWorkflowCount();
@@ -28,14 +34,6 @@ export const WorkflowCountCard = ({ classes, workflowCount, fetchWorkflowCount }
   return <DashboardNumCard displayNum={workflowCount} title={I18n.get('cardTitleTotalWorkflows')} icon={<Stars className={classes.cardIcon} />} />;
 };
 
-WorkflowCountCard.propTypes = {
-  classes: PropTypes.objectOf(PropTypes.string).isRequired,
-  workflowCount: PropTypes.number,
-  fetchWorkflowCount: PropTypes.func.isRequired,
-};
-WorkflowCountCard.defaultProps = {
-  workflowCount: null,
-};
-const mapStateToProps = state => ({ workflowCount: state.workflowCount });
+const mapStateToProps = (state: AppState) => ({ workflowCount: state.workflowCount });
 const mapDispatchToProps = { fetchWorkflowCount: fetchWorkflowCountAction };
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(WorkflowCountCard));
+export default connect(mapStateToProps, mapDispatchToProps)(memo(WorkflowCountCard));
