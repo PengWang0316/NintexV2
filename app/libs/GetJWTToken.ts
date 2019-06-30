2795f750e0846c36202eca57c1b33b668fe6dc6a
Kevin Wang <rocwang0316@gmail.com>
2019-06-30 10:48:53 -0700

    Added the return type

--- app/libs/GetJWTToken.ts
+++ app/libs/GetJWTToken.ts
@@ -1,6 +1,6 @@
 import { Auth } from 'aws-amplify';
 
-const getJwtToken = async () => {
+const getJwtToken = async (): Promise<string> => {
   const { idToken: { jwtToken } } = await Auth.currentSession() as any;
   return jwtToken;
 };
