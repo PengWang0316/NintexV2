dcf0ee69f08e23c7f706ab272107ffe284983ea5
Kevin Wang <rocwang0316@gmail.com>
2019-06-30 16:47:18 -0700

    Converted the PublisherCountCard component to the ts format.

--- app/components/cards/PublisherCountCard.tsx
+++ app/components/cards/PublisherCountCard.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import React, { useEffect, memo } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
 import { lightGreen } from '@material-ui/core/colors';
 import { SupervisedUserCircle } from '@material-ui/icons';
@@ -8,16 +7,23 @@ import I18n from '@kevinwang0316/i18n';
 
 import DashboardNumCard from './DashboardNumCard';
 import { fetchPublisherCount as fetchPublisherCountAction } from '../../store/PublisherCount/actions';
+import { AppState } from '../../store/ConfigureStore';
 
-const styles = {
+interface Props {
+  publisherCount: number | null;
+  fetchPublisherCount: () => void;
+}
+
+const useStyles = makeStyles({
   cardIcon: {
     fontSize: 36,
     color: lightGreen[500],
   },
-};
-let isFetching;
+});
+let isFetching = false;
 
-export const PublisherCountCard = ({ classes, publisherCount, fetchPublisherCount }) => {
+export const PublisherCountCard = ({ publisherCount = null, fetchPublisherCount }: Props) => {
+  const classes = useStyles({});
   useEffect(() => {
     if (!publisherCount && !isFetching) {
       fetchPublisherCount();
@@ -28,14 +34,6 @@ export const PublisherCountCard = ({ classes, publisherCount, fetchPublisherCoun
   return <DashboardNumCard displayNum={publisherCount} title={I18n.get('cardTitleWorkflowPublisher')} icon={<SupervisedUserCircle className={classes.cardIcon} />} />;
 };
 
-PublisherCountCard.propTypes = {
-  classes: PropTypes.objectOf(PropTypes.string).isRequired,
-  publisherCount: PropTypes.number,
-  fetchPublisherCount: PropTypes.func.isRequired,
-};
-PublisherCountCard.defaultProps = {
-  publisherCount: null,
-};
-const mapStateToProps = state => ({ publisherCount: state.publisherCount });
+const mapStateToProps = (state: AppState) => ({ publisherCount: state.publisherCount });
 const mapDispatchToProps = { fetchPublisherCount: fetchPublisherCountAction };
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(PublisherCountCard));
+export default connect(mapStateToProps, mapDispatchToProps)((memo)(PublisherCountCard));
