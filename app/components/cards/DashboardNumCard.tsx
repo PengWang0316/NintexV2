f429337c82402e6f836089aea797569af97fb75c
Kevin Wang <rocwang0316@gmail.com>
2019-06-30 15:51:14 -0700

    Converted the component to the ts format.

---
+++ app/components/cards/DashboardNumCard.tsx
@@ -0,0 +1,53 @@
+import React, { ReactNode, memo, useMemo } from 'react';
+import { Box, Card, CardContent } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  card: {
+    width: 255,
+    margin: '0 10px',
+  },
+  cardDiv: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  contentDiv: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+});
+// The regular experssion to help format numbers to have a comma in every three digitals
+const NUM_FORMATE_REGEXP = /(\d)(?=(\d{3})+(?!\d))/g;
+
+interface Props {
+  displayNum: string | number | null;
+  title: string;
+  icon: ReactNode;
+  extraContent: string | null;
+}
+
+export const DashboardNumCard = ({
+  displayNum = null, title, icon, extraContent = null,
+}: Props) => {
+  const classes = useStyles({});
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <div className={classes.contentDiv}>
+          <div>
+            <Box fontSize={14} color="textSecondary">
+              {title}
+            </Box>
+            <Box fontSize={26} fontWeight={1600} color="textSecondary">
+              {displayNum ? displayNum.toString().replace(NUM_FORMATE_REGEXP, '$1,') : '--'}
+            </Box>
+          </div>
+          {icon}
+        </div>
+        {extraContent}
+      </CardContent>
+    </Card>
+  );
+};
+export default memo(DashboardNumCard);
