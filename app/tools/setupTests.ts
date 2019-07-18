a679e1f217f788ebb113bbdda36bd6b4e2c8008c
Kevin Wang <rocwang0316@gmail.com>
2019-07-17 20:04:56 -0700

    Added the I18n setup in the setup file

--- app/tools/setupTests.ts
+++ app/tools/setupTests.ts
@@ -1,5 +1,11 @@
 // import 'raf/polyfill';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import I18n from '@kevinwang0316/i18n';
+
+import dict from '../Dict';
+
 
 configure({ adapter: new Adapter() });
+I18n.setDefaultLanguage('en');
+I18n.setDictionary(dict);
