cf1d23de434384f1bea7cef6dcdd5c850bf8a8e8
Kevin Wang <rocwang0316@gmail.com>
2019-07-22 20:34:27 -0700

    Added the mocked component and added the snapshot file.

---
+++ __tests__/components/containers/ManagementContainer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+
+import { ManagementContainer } from '../../../app/components/containers/ManagementContainer';
+import JssProviderWrapper from '../../libs/JssProviderWrapper';
+
+jest.mock('../../../app/components/WorkflowTable', () => 'WorkflowTable');
+
+describe('ManagementContainer component', () => {
+  const defaultProps = {
+    user: null,
+    currentAuthenticatedUser: jest.fn(),
+  };
+  beforeEach(() => jest.clearAllMocks());
+
+  test('Snapshot without user', () => expect(renderer.create(JssProviderWrapper(<ManagementContainer {...{ ...defaultProps }} />)).toJSON()).toMatchSnapshot());
+  test('Snapshot with user', () => expect(renderer.create(JssProviderWrapper(<ManagementContainer {...{ ...defaultProps, user: { nickname: 'name' } }} />)).toJSON()).toMatchSnapshot());
+});
