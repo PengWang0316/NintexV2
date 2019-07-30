d1d2ff4ca6dd02e81ec841fa5c15497489b3a2c7
Kevin Wang <rocwang0316@gmail.com>
2019-07-29 20:10:32 -0700

    Added one snapshot test for the AddTagDialog component.

--- __tests__/components/AddTagDialog.tsx
+++ __tests__/components/AddTagDialog.tsx
@@ -4,6 +4,16 @@ import renderer from 'react-test-renderer';
 import { AddTagDialog } from '../../app/components/AddTagDialog';
 import JssProviderWrapper from '../libs/JssProviderWrapper';
 
+jest.mock('react-color', () => ({ CirclePicker: 'CirclePicker' }));
+jest.mock('@material-ui/core/Button', () => 'Button');
+jest.mock('@material-ui/core/Dialog', () => 'Dialog');
+jest.mock('@material-ui/core/DialogActions', () => 'DialogActions');
+jest.mock('@material-ui/core/DialogCOntent', () => 'DialogCOntent');
+jest.mock('@material-ui/core/DialogContentText', () => 'DialogContentText');
+jest.mock('@material-ui/core/DialogTitle', () => 'DialogTitle');
+jest.mock('@material-ui/core/Chip', () => 'Chip');
+jest.mock('@material-ui/core/TextField', () => 'TextField');
+
 describe('AddTagDialog Component', () => {
   const defaultProps = {
     open: true,
