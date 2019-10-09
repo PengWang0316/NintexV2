27befa047cde1aef9242cc23695b88e24b9cf0fc
Kevin Wang <rocwang0316@gmail.com>
2019-10-08 20:25:56 -0700

    Removed unnecessary code

--- __tests__/components/AddTagDialog.tsx
+++ __tests__/components/AddTagDialog.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { render } from '@testing-library/react';
 import { shallow } from 'enzyme';
 
 import { AddTagDialog } from '../../app/components/AddTagDialog';
